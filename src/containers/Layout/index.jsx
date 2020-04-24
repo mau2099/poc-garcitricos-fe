0f887886f955ad006d6672dbe7a99ca636733dcd
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-24 17:06:30 -0500

    -> Babel initialization -> Webpack mode production initialization -> Readme added -> Empty Components / Containers added

---
+++ src/containers/Layout/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Layout = () => {
+  <h1>Garcitricos' app</h1>
+
+}
+
+export default Layout;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react-dom';
+import ReactDOM from 'react-dom'
+
+import Layout from './containers/Layout'
+
+ReactDOM.render(<Layout />, document.getElementById(app));
