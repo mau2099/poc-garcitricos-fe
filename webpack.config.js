dc5d2e017839bc0d65bd513d4a3ac14f2b1c8d35
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-27 11:06:25 -0500

    -> SASS webpack loader config * Estructura de carpetas de Componentes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -26,12 +27,24 @@ module.exports = {
           loader: 'html-loader',
         },
       },
+
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
     }),
   ],
 };
